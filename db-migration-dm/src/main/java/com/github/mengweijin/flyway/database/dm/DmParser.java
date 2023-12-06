ed9722592653465072a3fcc8f64bd45da4598035
Jack Chu <chuchuanbao@gmail.com>
2023-12-06 14:22:05 +0800

    feat(issues/I8K7CG): 修复DROP TABLE IF EXISTS xxx语句解析异常

--- db-migration-dm/src/main/java/com/github/mengweijin/flyway/database/dm/DmParser.java
+++ db-migration-dm/src/main/java/com/github/mengweijin/flyway/database/dm/DmParser.java
@@ -2,14 +2,34 @@ package com.github.mengweijin.flyway.database.dm;
 
 import org.flywaydb.core.api.configuration.Configuration;
 import org.flywaydb.core.internal.database.oracle.OracleParser;
+import org.flywaydb.core.internal.parser.ParserContext;
 import org.flywaydb.core.internal.parser.ParsingContext;
+import org.flywaydb.core.internal.parser.PeekingReader;
+import org.flywaydb.core.internal.parser.Token;
+
+import java.util.List;
 
 /**
  * @author mengweijin
  */
 public class DmParser extends OracleParser {
+
+    private static final String IF = "IF";
+    private static final String EXISTS = "EXISTS";
+
+
     public DmParser(Configuration configuration, ParsingContext parsingContext) {
         super(configuration, parsingContext);
     }
 
+    @Override
+    protected void adjustBlockDepth(ParserContext context, List<Token> tokens, Token keyword, PeekingReader reader) {
+        String keywordText = keyword.getText();
+        Token previousToken = getPreviousToken(tokens, keyword.getParensDepth());
+        if (EXISTS.equalsIgnoreCase(keywordText) && IF.equalsIgnoreCase(previousToken.getText())) {
+            context.decreaseBlockDepth();
+        } else {
+            super.adjustBlockDepth(context, tokens, keyword, reader);
+        }
+    }
 }
