da843be82aca961595219467d6f1d932ac34abbc
Jack Chu <chuchuanbao@gmail.com>
2023-12-13 10:45:28 +0800

    feat(IF_EXISTS_OR_NOT_EXISTS): 优化对于(CREATE|DROP) XXXX IF (NOT) EXISTS的解析

--- db-migration-dm/src/main/java/com/github/mengweijin/flyway/database/dm/DmParser.java
+++ db-migration-dm/src/main/java/com/github/mengweijin/flyway/database/dm/DmParser.java
@@ -8,6 +8,7 @@ import org.flywaydb.core.internal.parser.PeekingReader;
 import org.flywaydb.core.internal.parser.Token;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author mengweijin
@@ -17,6 +18,8 @@ public class DmParser extends OracleParser {
     private static final String IF = "IF";
     private static final String EXISTS = "EXISTS";
 
+    private static final String CREATE_OR_DROP_IF_OR_NOT = "(CREATE|DROP)\\s(TABLE|PROCEDURE|FUNCTION)\\sIF(\\sNOT)?";
+
 
     public DmParser(Configuration configuration, ParsingContext parsingContext) {
         super(configuration, parsingContext);
@@ -25,11 +28,20 @@ public class DmParser extends OracleParser {
     @Override
     protected void adjustBlockDepth(ParserContext context, List<Token> tokens, Token keyword, PeekingReader reader) {
         String keywordText = keyword.getText();
-        Token previousToken = getPreviousToken(tokens, keyword.getParensDepth());
-        if (EXISTS.equalsIgnoreCase(keywordText) && IF.equalsIgnoreCase(previousToken.getText())) {
-            context.decreaseBlockDepth();
-        } else {
-            super.adjustBlockDepth(context, tokens, keyword, reader);
+        if (EXISTS.equals(keywordText) && context.getBlockDepth() > 0) {
+            String sql = makeSql(tokens);
+            if (sql.matches(CREATE_OR_DROP_IF_OR_NOT)) {
+                context.decreaseBlockDepth();
+                return;
+            }
         }
+        super.adjustBlockDepth(context, tokens, keyword, reader);
+    }
+
+    private String makeSql(List<Token> tokens) {
+        return tokens
+                .stream()
+                .map(Token::getText)
+                .collect(Collectors.joining(" "));
     }
 }
