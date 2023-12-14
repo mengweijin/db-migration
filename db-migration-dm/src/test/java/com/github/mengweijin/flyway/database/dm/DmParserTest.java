bfb2a0e3d407fec8a1e184adc8250198316f0fb7
Jack Chu <chuchuanbao@gmail.com>
2023-12-14 10:25:05 +0800

    test(parser): 优化测试用例

--- db-migration-dm/src/test/java/com/github/mengweijin/flyway/database/dm/DmParserTest.java
+++ db-migration-dm/src/test/java/com/github/mengweijin/flyway/database/dm/DmParserTest.java
@@ -2,10 +2,10 @@ package com.github.mengweijin.flyway.database.dm;
 
 
 import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.Location;
+import org.flywaydb.core.api.configuration.Configuration;
 import org.flywaydb.core.api.resource.LoadableResource;
 import org.flywaydb.core.internal.parser.ParsingContext;
-import org.flywaydb.core.internal.scanner.filesystem.FileSystemScanner;
+import org.flywaydb.core.internal.resource.filesystem.FileSystemResource;
 import org.flywaydb.core.internal.sqlscript.SqlStatementIterator;
 import org.junit.Test;
 
@@ -15,19 +15,19 @@ public class DmParserTest {
 
     @Test
     public void testParse() {
-        FileSystemScanner fileSystemScanner = new FileSystemScanner(Charset.defaultCharset(), false, false, true);
-        Flyway flyway = Flyway.configure()
-                .dataSource("jdbc:dm://localhost:5236", "SYSDBA", "")
-                .locations("src/test/resources/db/migration")
-                .load();
-        Location[] locations = flyway.getConfiguration().getLocations();
-        DmParser parser = new DmParser(flyway.getConfiguration(), new ParsingContext());
-        for (LoadableResource scanForResource : fileSystemScanner.scanForResources(locations[0])) {
-            SqlStatementIterator sqlStatementIterator = parser.parse(scanForResource);
-            while (sqlStatementIterator.hasNext()) {
-                System.out.println(sqlStatementIterator.next().getSql());
-                System.out.println("================");
-            }
+        Flyway flyway = Flyway.configure().load();
+        Configuration configuration = flyway.getConfiguration();
+        DmParser parser = new DmParser(configuration, new ParsingContext());
+        LoadableResource scanForResource = new FileSystemResource(
+                configuration.getLocations()[0],
+                "src/test/resources/db/migration/V1__demo.sql",
+                Charset.defaultCharset(),
+                false
+        );
+        SqlStatementIterator sqlStatementIterator = parser.parse(scanForResource);
+        while (sqlStatementIterator.hasNext()) {
+            System.out.println(sqlStatementIterator.next().getSql());
+            System.out.println("================");
         }
     }
 }
