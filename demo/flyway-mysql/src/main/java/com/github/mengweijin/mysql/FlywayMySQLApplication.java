30a379869b2db1248c8c3208e641c1e656d66b18
mengweijin <1002284406@qq.com>
2024-09-05 18:59:54 +0800

    demo flyway mysql and postgresql.

---
+++ demo/flyway-mysql/src/main/java/com/github/mengweijin/mysql/FlywayMySQLApplication.java
@@ -0,0 +1,15 @@
+package com.github.mengweijin.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengweijin
+ */
+@SpringBootApplication
+public class FlywayMySQLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayMySQLApplication.class, args);
+	}
+}
