e30af4270d67b3ea769cdc1ccdd0671445d4de69
mengweijin <1002284406@qq.com>
2024-09-21 06:36:43 +0800

    demo for github issue 8

---
+++ demo/liquibase-dm-spring-boot2-github-issue-8/src/main/java/com/github/mengweijin/liquibase/LiquibaseDMGithubIssue8Application.java
@@ -0,0 +1,15 @@
+package com.github.mengweijin.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengweijin
+ */
+@SpringBootApplication
+public class LiquibaseDMGithubIssue8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDMGithubIssue8Application.class, args);
+	}
+}
