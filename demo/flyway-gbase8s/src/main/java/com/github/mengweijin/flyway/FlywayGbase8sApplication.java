3b5bb4c935ec2c166f2ab64fb0e0d106e42b8f0c
mengweijin <1002284406@qq.com>
2024-10-12 05:14:23 +0800

    version 1.2.0-snapshot

---
+++ demo/flyway-gbase8s/src/main/java/com/github/mengweijin/flyway/FlywayGbase8sApplication.java
@@ -0,0 +1,15 @@
+package com.github.mengweijin.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengweijin
+ */
+@SpringBootApplication
+public class FlywayGbase8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayGbase8sApplication.class, args);
+	}
+}
---
+++ demo/flyway-gbase8s/src/main/resources/db/migration/gbase/V2020.12.11__ddl_script.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS SYS_USER;
+
+CREATE TABLE SYS_USER (
+    ID                              VARCHAR2(36) NOT NULL,
+    USERNAME                        VARCHAR2(255) NULL,
+    PASSWORD                        VARCHAR2(255) NULL,
+    CREATE_BY                       VARCHAR2(255) NULL,
+    CREATE_TIME                     TIMESTAMP(6) NULL,
+    UPDATE_BY                       VARCHAR2(255) NULL,
+    UPDATE_TIME                     TIMESTAMP(6) NULL,
+    PRIMARY KEY (ID)
+);
+
+COMMENT ON TABLE SYS_USER                                IS '用户表';
+COMMENT ON COLUMN SYS_USER.ID                            IS '主键ID';
+COMMENT ON COLUMN SYS_USER.USERNAME                      IS '用户名';
+COMMENT ON COLUMN SYS_USER.PASSWORD                      IS '密码';
+COMMENT ON COLUMN SYS_USER.CREATE_BY                     IS '创建者';
+COMMENT ON COLUMN SYS_USER.CREATE_TIME                   IS '创建时间';
+COMMENT ON COLUMN SYS_USER.UPDATE_BY                     IS '更新者';
+COMMENT ON COLUMN SYS_USER.UPDATE_TIME                   IS '更新时间';
+
+insert into SYS_USER values('1', 'admin', '123456', 'SYSTEM', sysdate, 'SYSTEM', sysdate);
