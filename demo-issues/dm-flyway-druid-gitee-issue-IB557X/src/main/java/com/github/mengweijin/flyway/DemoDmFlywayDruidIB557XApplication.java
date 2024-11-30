41710d387e795bd8fce38321ed5b286652f2bb86
mengweijin <1002284406@qq.com>
2024-11-30 19:50:49 +0800

    issue

---
+++ demo-issues/dm-flyway-druid-gitee-issue-IB557X/src/main/java/com/github/mengweijin/flyway/DemoDmFlywayDruidIB557XApplication.java
@@ -0,0 +1,15 @@
+package com.github.mengweijin.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengweijin
+ */
+@SpringBootApplication
+public class DemoDmFlywayDruidIB557XApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDmFlywayDruidIB557XApplication.class, args);
+	}
+}
