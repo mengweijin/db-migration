9fe349ab0750b0172356b8921bf59a6b2103ec76
mengweijin <1002284406@qq.com>
2025-10-25 17:13:53 +0800

    github issue 19 demo

---
+++ demo-issues/dm-flyway-github-issue-19/src/main/java/com/github/mengweijin/flyway/DemoDmFlywayGithub19Application.java
@@ -0,0 +1,15 @@
+package com.github.mengweijin.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengweijin
+ */
+@SpringBootApplication
+public class DemoDmFlywayGithub19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDmFlywayGithub19Application.class, args);
+	}
+}
---
+++ demo-issues/dm-flyway-github-issue-19/src/main/resources/db/migration/dm/V2025.01.01__insert.sql
@@ -0,0 +1,4 @@
+
+insert into SYS_USER values('1001', 'admin1001', '123456', 'SYSTEM', sysdate, 'SYSTEM', sysdate);
+insert into SYS_USER values('1002', 'admin1002', '123456', 'SYSTEM', sysdate, 'SYSTEM', sysdate);
+insert into SYS_USER values('1003', 'admin1003', '123456', 'SYSTEM', sysdate, 'SYSTEM', sysdate);
